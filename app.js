be8c71088fac8685c637740eb8035cc3e1789adf
busybiee1 <busybiee1@gmail.com>
2024-12-04 22:09:38 -0600

    update required finished about to start bonus items

--- app.js
+++ app.js
@@ -665,104 +665,232 @@ console.log("\n");
 //////////////////////////////////////////////////////////////////////////////////
 
 // Objects
-// Let's set up an object data structure. Let's say we have a website that sells products, and we have a user of our website, and we want to store that user's data. The object data structure is a good way to organize the data from our user.
+// Let's set up an object data structure. Let's say we have a website that sells products, 
+// and we have a user of our website, and we want to store that user's data. 
+// The object data structure is a good way to organize the data from our user.
 // A. Make a user object
 // Create an object called user.
-// Write in to the object the key-value pairs for name, email, age, and purchased. Set the value of purchasedto an empty array []. Set the other values to whatever you would like.
+// Write in to the object the key-value pairs for name, email, age, and purchased. 
+// Set the value of purchasedto an empty array []. Set the other values to whatever you would like.
 // ANS:
+const user = {
+    name: "john",
+    age: 33,
+    email: "john@john.com",
+    purchased: [] 
+  };
+  console.log(user);
+  console.log("\n");
 
 /////////////////////////////////////////////////////////////////////////////////
 
 // B. Update the user
-// Our user has changed his or her email address. Without changing the original userobject, update the emailvalue to a new email address.
-// Our user has had a birthday! Without changing the original userobject, increment the agevalue using the postfix operator. Hint: age++
+// Our user has changed his or her email address. Without changing the original userobject, 
+// update the emailvalue to a new email address.
+// Our user has had a birthday! Without changing the original userobject, increment the 
+// agevalue using the postfix operator. Hint: age++
 // ANS:
+const updatedUserObjemail = {
+    ...user, 
+    email: "newemail@example.com", 
+};
+updatedUserObjemail.age++
+console.log(updatedUserObjemail);
+console.log("\n");
 
 /////////////////////////////////////////////////////////////////////////////////
 
 // C. Adding keys and values
 // You have decided to add your user's location to the data that you want to collect.
-// Without changing the original userobject, add a new key locationto the object, and give it a value or some-or-other location (a string).
+// Without changing the original userobject, add a new key location to the object, 
+// and give it a value or some-or-other location (a string).
 // ANS:
-
+updatedUserObjemail["location"] = "somelocation";
+console.log(updatedUserObjemail);
+console.log("\n");
 /////////////////////////////////////////////////////////////////////////////////
 
 // D. Shopaholic!
-// Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchasedarray.
-// Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchasedarray.
-// Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchasedarray.
+// Our user has purchased an item! They have purchased some "carbohydrates". 
+// Using .push(), add the string "carbohydrates" to the purchased array.
+// ANS:
+user["purchased"].push("carbohydrates") 
+console.log(user);
+console.log("\n");
+// Our user has purchased an item! They have purchased some "peace of mind". 
+// Using .push(), add the string "peace of mind" to the purchasedarray.
+// ANS:
+user["purchased"].push("peace of mind") 
+console.log(user);
+console.log("\n");
+
+// Our user has purchased an item! They have purchased some "Merino jodhpurs". 
+// Using .push(), add the string "Merino jodhpurs" to the purchasedarray.
+user["purchased"].push("Merino jodhpurs") 
+console.log(user);
+console.log("\n");
+
 // Console.log just the "Merino jodhpurs" from the purchasedarray.
 // ANS:
+console.log(user["purchased"][2]);
+console.log("\n");
 
 /////////////////////////////////////////////////////////////////////////////////
 
 // E. Object-within-object
-// Remember that you can add an object to an existing object in the same way that you can add any new property/value pair.
-// If we want to give our user a friendwith a nameand age, we could write:
+// Remember that you can add an object to an existing object in the same way that 
+// you can add any new property/value pair.
+// If we want to give our user a friend with a name and age, we could write:
 // user.friend = {
 //     name: "Grace Hopper",
 //     age: 85
 // }
-// When we console.log user, we would see the friendobject added to our user object.
+// When we console.log user, we would see the friend object added to our user object.
+// Write a friendobject into your userobject and give the friend a name, age, location, 
+// and purchased array (empty for now)
 // ANS:
+user["friend"] = {
+        name: "Grace Hopper",
+        age: 85,
+        location: "NYC",
+        purchased: [] 
+    }
+console.log(user);
+console.log("\n");
 
 /////////////////////////////////////////////////////////////////////////////////
 
-// Write a friendobject into your userobject and give the friend a name, age, location, and purchased array (empty for now)
 // Console.log just the friend's name
+// ANS
+console.log(user.friend.name);
+console.log(user.friend["name"]);
+console.log("\n");
+
+/////////////////////////////////////////////////////////////////////////////////
+
 // Console.log just the friend's location
+// ANS
+console.log(user.friend.location);
+console.log(user.friend["location"]);
+console.log("\n");
+
+/////////////////////////////////////////////////////////////////////////////////
+
 // CHANGE the friend's age to 55
+// ANS
+user.friend.age = 55;
+console.log(user);
+console.log(user.friend);
+console.log("\n");
+
+/////////////////////////////////////////////////////////////////////////////////
+
 // The friendhas purchased "The One Ring". Use .push()to add "The One Ring" to the friend's purchasedarray.
+// ANS:
+user.friend.purchased.push("The One Ring");
+console.log(user.friend);
+console.log("\n");
+
+/////////////////////////////////////////////////////////////////////////////////
+
 // The friendhas purchased "A latte". Use .push()to add "A latte" to the friend's purchasedarray.
 // Console.log just "A latte" from the friend's purchasedarray.
+// ANS:
+user.friend.purchased.push("A latte");
+console.log(user.friend);
+console.log(user.friend.purchased[1]);
+console.log("\n");
 
+/////////////////////////////////////////////////////////////////////////////////
 
 // F. Loops
-// Write a for loop that iterates over the User's purchasedarray (NOT the friend's purchased array), and prints each element to the console.
-// Write a for loop that iterates over the Friend's purchasedarray, and prints each element to the console.
+// Write a for loop that iterates over the User's purchased array (NOT the friend's purchased array), 
+// and prints each element to the console.
+// ANS:
+user.purchased.forEach(item => {
+    console.log(item)
+})
+console.log("\n");
 
+/////////////////////////////////////////////////////////////////////////////////
 
+// Write a for loop that iterates over the Friend's purchasedarray, 
+// and prints each element to the console.
+// ANS:
+user.friend.purchased.forEach(item => {
+    console.log(item)
+})
+console.log("\n");
+
+/////////////////////////////////////////////////////////////////////////////////
 
 // G. Functions can operate on objects
-// Write a single function updateUserthat takes no parameters. When the function is run, it should:
+// Write a single function updateUser that takes no parameters. When the function is run, it should:
 // it should increment the user's age by 1
 // make the user's name uppercase
 // The function does not need a returnstatement, it will merely modify the user object.
+// ANS:
+function updateUser() {
+    user.age++;
+    user.name = user.name.toUpperCase();
+}
+updateUser();
+console.log(user);
+console.log("\n");
 
-// Write a function oldAndLoudthat performs the exact same tasks as updateUser, but instead of hard-coding it to only work on our userobject, make it take a parameter person, and have it modify the object that is passed in as an argument when the function is called. Call your oldAndLoudfunction with useras the argument.
+// Write a function oldAndLoud that performs the exact same tasks as updateUser, 
+// but instead of hard-coding it to only work on our userobject, 
+// make it take a parameter person, and have it modify the object that is passed 
+// in as an argument when the function is called. 
+// Call your oldAndLoud function with user as the argument.
+// ANS:
+function oldAndLoud(person) {
+    person.age++;
+    person.name = person.name.toUpperCase();
+}
+oldAndLoud(user); 
+console.log(user);
+console.log("\n");
 
-
-
-
-
+/////////////////////////////////////////////////////////////////////////////////
 
 // Requirements Complete! Hungry for More?
 
-
-
 // Cat Combinator
-
-
-
 // 1. Mama cat
-// Define an object called cat1that contains the following properties:
-
+// Define an object called cat1 that contains the following properties:
 // name
 // breed
 // age (a number)
 // console.log the cat's age
 // console.log the cat's breed
+// ANS:
+const cat1 = {
+    name: "tina",
+    breed: "bengal",
+    age: 3
+}
+console.log(cat1.age);
+console.log(cat1.breed);
+console.log("\n");
 
-
+/////////////////////////////////////////////////////////////////////////////////
 
 // 2. Papa cat
 // Define an object called cat2that also contains the properties:
-
 // name
 // breed
 // age (a number)
+// ANS:
+const cat2 = {
+    name: "tom",
+    breed: "ferrel",
+    age: 4
+}
+console.log(cat2);
+console.log("\n");
 
-
+/////////////////////////////////////////////////////////////////////////////////
 
 // 3. Combine Cats!
 // The cats are multiplying!
